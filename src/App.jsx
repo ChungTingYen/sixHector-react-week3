265ac9f4ceb516d48c1efd9bfe3a29adbd7bb922
AllanYen <iamallan0917@gmail.com>
2025-01-03 17:40:08 +0800

    add apiservice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import  { adminInstance } from "./apiconfig";
+import * as apiService from "./apiService/apiService";
 
 function App() {
-
+  const [productData,setProductData] = useState([]);
+  useEffect(()=>{
+    const getProductData = async()=>{
+      const response = await apiService.axiosGetProductData('/products/all');
+      const { products } = response;
+      setProductData(products);
+      console.log(response);
+    };
+    getProductData();
+  },[]);
   return (
     <>
+      <pre>{JSON.stringify(productData,null,2)}</pre>
       <p className="read-the-docs">
         week2
       </p>
---
+++ src/apiService/apiService.js
@@ -0,0 +1,6 @@
+import { adminInstance } from "../apiconfig";
+
+export const  axiosGetProductData = async(path)=>{
+  const response = await adminInstance.get(path);
+  return response.data;
+};
