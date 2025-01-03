a758cd4c98687b8ef93966caf15d2cbaca14d341
AllanYen <iamallan0917@gmail.com>
2025-01-03 16:49:01 +0800

    sencond commit 202401051648

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        week2
+      </p>
+    </>
+  );
+}
+
+export default App;
