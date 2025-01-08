1232af1de37c6e84e7f71ee7d3a869dcb32afeca
AllanYen <iamallan0917@gmail.com>
2025-01-08 11:11:03 +0800

    202501081111

--- src/note.js
+++ src/note.js
@@ -2,9 +2,6 @@
 //以下紀錄舊寫法，連續post時高機率會出現驗證錯誤
 const handleDeleteAllProducts = async (productData) => {
   const headers = getHeadersFromCookie();
-  AppModalRef.current.open();
-  AppModalRef.current.setModalImage(null);
-  AppModalRef.current.toggleFooter(false);
   try {
     const res = await Promise.all(
       productData.map(async (data, index) => {
@@ -16,20 +13,15 @@ const handleDeleteAllProducts = async (productData) => {
         );
       })
     );
-    AppModalRef.current.close();
     alert('所有產品都已成功刪除');
   } catch (error) {
     console.error("刪除產品時發生錯誤：", error);
     if (error.request.response.message) alert(error.request.response.message);
     else alert(error.response.data.message);
-    AppModalRef.current.close();
   }
 };
 
 const handleAddAllProducts = async (productDataAtLocal) => {
-  AppModalRef.current.open();
-  AppModalRef.current.setModalImage(null);
-  AppModalRef.current.toggleFooter(false);
   const headers = getHeadersFromCookie();
 
   try {
@@ -49,11 +41,9 @@ const handleAddAllProducts = async (productDataAtLocal) => {
     console.log('所有產品都已成功上傳：', resProducts);
     await getProductData(null, headers, setProductData);
     setTempProduct(null);
-    AppModalRef.current.close();
   } catch (error) {
     console.error("上傳產品時發生錯誤：", error);
     if (error.request.response.message) alert(error.request.response.message);
     else alert(error.response.data.message);
-    AppModalRef.current.close();
   }
 };
